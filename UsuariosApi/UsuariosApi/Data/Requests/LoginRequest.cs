f74afd2b5b303ed39ecf744eb597b46f63e2150f
Samuca2003 <112135974+Samuca2003@users.noreply.github.com>
2022-11-15 09:52:18 -0300

    Add files via upload
    *Observação: A pasta bin tirei do projeto, pois o git tem limitação de arquivos no repositório.

---
+++ UsuariosApi/UsuariosApi/Data/Requests/LoginRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsuariosApi.Data.Requests
+{
+    public class LoginRequest
+    {
+        [Required]
+
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+
+
+
+
+
+    }
+}
---
+++ UsuariosApi/UsuariosApi/Models/Token.cs
@@ -0,0 +1,16 @@
+﻿namespace UsuariosApi.Models
+{
+    public class Token
+    {
+        public Token(string value)
+        {
+            Value = value;
+        }
+
+        public string Value { get;}
+
+
+
+
+    }
+}
